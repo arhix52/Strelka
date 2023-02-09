fa531da29fbd0263d79767ad865cc2419131ee80
arhix52 <121569944+arhix52@users.noreply.github.com>
2023-02-09 15:57:51 +0400

    Fix: resizing with OptiX (#30)

--- src/render/optix/OptixBuffer.cpp
+++ src/render/optix/OptixBuffer.cpp
@@ -16,15 +16,27 @@ oka::OptixBuffer::OptixBuffer(void* devicePtr, BufferFormat format, uint32_t wid
 oka::OptixBuffer::~OptixBuffer()
 {
     // TODO:
+    if (mDeviceData)
+    {
+        cudaFree(mDeviceData);
+    }
 }
 
 void oka::OptixBuffer::resize(uint32_t width, uint32_t height)
 {
+    if (mDeviceData)
+    {
+        cudaFree(mDeviceData);
+    }
+    mWidth = width;
+    mHeight = height;
+    const size_t bufferSize = mWidth * mHeight * getElementSize();
+    cudaMalloc(reinterpret_cast<void**>(&mDeviceData), bufferSize);
 }
 
 void* oka::OptixBuffer::map()
 {
-    const size_t bufferSize =mWidth * mHeight * getElementSize();
+    const size_t bufferSize = mWidth * mHeight * getElementSize();
     mHostData.resize(bufferSize);
     cudaMemcpy(static_cast<void*>(mHostData.data()), mDeviceData, bufferSize, cudaMemcpyDeviceToHost);
     return nullptr;
