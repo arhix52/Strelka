248c7cb74341e047faca66b3be75d6e96ab43322
Arhix <arhix52@gmail.com>
2023-05-05 15:42:58 +0200

    smart selection for function generation

--- src/materialmanager/mdlPtxCodeGen.cpp
+++ src/materialmanager/mdlPtxCodeGen.cpp
@@ -140,8 +140,12 @@ bool MdlPtxCodeGen::appendMaterialToLinkUnit(uint32_t idx,
     auto emissionFuncName = std::string(EMISSION_FUNC_NAME) + "_" + idxStr;
     auto emissionIntensityFuncName = std::string(EMISSION_INTENSITY_FUNC_NAME) + "_" + idxStr;
 
+    // Here we need to detect if current material for hair, if so, replace name in function description
+    mi::base::Handle<mi::neuraylib::IExpression const> hairExpr(compiledMaterial->lookup_sub_expression("hair"));
+    const bool isHair = hairExpr != nullptr;
+
     std::vector<mi::neuraylib::Target_function_description> genFunctions;
-    genFunctions.push_back(mi::neuraylib::Target_function_description("hair", scatteringFuncName.c_str()));
+    genFunctions.push_back(mi::neuraylib::Target_function_description( isHair ? "hair" : "surface.scattering", scatteringFuncName.c_str()));
     genFunctions.push_back(
         mi::neuraylib::Target_function_description("surface.emission.emission", emissionFuncName.c_str()));
     genFunctions.push_back(
