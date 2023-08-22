3656e46fa96d2b0fdaa65b2a426735196edab758
Arhix <arhix52@gmail.com>
2023-08-22 10:11:53 +0200

    fixed settings

--- include/settings/settings.h
+++ include/settings/settings.h
@@ -62,4 +62,34 @@ private:
     }
 };
 
+template<>
+inline void SettingsManager::setAs(const char* name, const std::string& value)
+{
+    mMap[name] = value;
+}
+
+template <>
+inline bool SettingsManager::convertValue(const std::string& value)
+{
+    return static_cast<bool>(atoi(value.c_str()));
+}
+
+template <>
+inline float SettingsManager::convertValue(const std::string& value)
+{
+    return static_cast<float>(atof(value.c_str()));
+}
+
+template <>
+inline uint32_t SettingsManager::convertValue(const std::string& value)
+{
+    return static_cast<uint32_t>(atoi(value.c_str()));
+}
+
+template <>
+inline std::string SettingsManager::toString(const std::string& value)
+{
+    return value;
+}
+
 } // namespace oka
