a8263008ea9cf30029727564f9ac33f1cf9d6b3a
Arhix <arhix52@gmail.com>
2023-09-02 16:58:44 +0200

    war log for linux

--- src/log/logmanager.cpp
+++ src/log/logmanager.cpp
@@ -3,6 +3,7 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/cfg/env.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 #include <memory>
 #include <vector>
 
@@ -23,8 +24,13 @@ void oka::Logmanager::initialize()
     {
         spdlog::cfg::load_env_levels();
         auto consolesink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        std::vector<spdlog::sink_ptr> sinks = { consolesink };
-        auto logger = std::make_shared<spdlog::logger>("Strelka", sinks.begin(), sinks.end());
+        auto fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("strelka.log");
+        std::vector<spdlog::sink_ptr> sinks = { consolesink, fileSink };
+        logger = std::make_shared<spdlog::logger>("Strelka", sinks.begin(), sinks.end());
+
+        // TODO: env var doesn't work on linux
+        logger->set_level(spdlog::level::trace);
+        logger->flush_on(spdlog::level::trace);
 
 #if defined(WIN32)
         logger->set_level(spdlog::level::trace);
