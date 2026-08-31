9936aa70f631b9a4d2e265f32dc455b26fb8e0bd
Arhix <arhix52@gmail.com>
2026-08-31 17:35:24 +0200

    Remove orphan source files superseded by the live build.

--- src/display/metal/metalcpphelp.cpp
+++
@@ -1,6 +0,0 @@
-#define NS_PRIVATE_IMPLEMENTATION
-#define MTL_PRIVATE_IMPLEMENTATION
-// #define MTK_PRIVATE_IMPLEMENTATION
-#define CA_PRIVATE_IMPLEMENTATION
-#include <Metal/Metal.hpp>
-#include <QuartzCore/QuartzCore.hpp>
